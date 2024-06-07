f8c18645d52bae99ae01e18e2217f2799cd6edfc
ilker <ilker@ILKEROZAN>
2024-06-07 14:50:09 +0300

    Add project files.

---
+++ RabbitMQ.CreateExcel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RabbitMQ.CreateExcel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
