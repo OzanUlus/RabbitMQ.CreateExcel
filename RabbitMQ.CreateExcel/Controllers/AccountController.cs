3767060f18e4732921c4b2358392c8fa3171ed98
Ozan Ulus <ilkerozanulus@gmail.com>
2024-06-07 23:37:16 +0300

    created login view and controller

---
+++ RabbitMQ.CreateExcel/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RabbitMQ.CreateExcel.Controllers
+{
+   
+
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(string Email , string Password) 
+        {
+            var user = await _userManager.FindByEmailAsync(Email);
+            if (user == null) 
+            {
+               ModelState.AddModelError("","Kullanıcı bulunumadı");
+                return View();
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(user,Password,true ,false);
+            if (!result.Succeeded) 
+            {
+                ModelState.AddModelError("", "Kullanıcı adı veya şifre hatalı");
+                return View();
+            }
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
